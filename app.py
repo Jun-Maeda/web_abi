1b6192274c9ec980e822faf31efb77c4d6088dc5
maeda <jun_pure@icloud.com>
2019-12-01 14:33:06 +0900

    取得したデータをきちんと文字で出力できるようにした。

--- app.py
+++ app.py
@@ -3,5 +3,12 @@ import requests
 zip = input('郵便番号を入力してください。')
 
 response = requests.get(f'http://zipcloud.ibsnet.co.jp/api/search?zipcode={zip}')
-print(response.text)
+zip_json = response.json()
+zip_address = zip_json['results'][0]
+都道府県 = zip_address['address1']
+市町村 = zip_address['address2']
+地域 = zip_address['address3']
+address = f'{都道府県}{市町村}{地域}'
+print(address)
+
 
