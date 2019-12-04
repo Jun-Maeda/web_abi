a2ebd39d518e719ea0986cbe363f7a442018de38
maeda <jun_pure@icloud.com>
2019-12-04 21:26:44 +0900

    ファイルついか

---
+++ sample01.py
@@ -0,0 +1,6 @@
+def double(number: int) -> int:
+    return number * 2
+
+
+print(double(3))
+print(double(9))
---
+++ sample02.py
@@ -0,0 +1,4 @@
+from sample01 import double
+number = 5
+
+print(f'{number}の二倍は{double(number)}')
